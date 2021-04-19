f558be0070e9064a8ac4f9f740ea3fa33d7b8882
Ilya Taskaev <ansustiwaz@gmail.com>
2021-04-19 20:52:14 +0300

    init

---
+++ main.tf
@@ -0,0 +1,12 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.27"
+    }
+  }
+}
+provider "aws" {
+  profile = "default"
+  region  = "eu-north-1"
+}

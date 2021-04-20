b235af2798602b026cf1c28db01e9678be60f631
Ilya Taskaev <ansustiwaz@gmail.com>
2021-04-20 10:33:54 +0300

    added bucket and ec2 instance

--- main.tf
+++ main.tf
@@ -10,3 +10,38 @@ provider "aws" {
   profile = "default"
   region  = "eu-north-1"
 }
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"] 
+    }
+
+  owners = ["099720109477"] # Canonical
+                               }
+
+resource "aws_instance" "example" {
+  count         = 1
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t3.micro"
+  key_name      = "EC2_Tutor"
+
+  tags = {
+    Name = "test"
+  }
+}
+
+resource "aws_s3_bucket" "name" {
+}
+
+output "aws_s3_bucket_id" {
+  description = "Public IP address of the EC2 instance"
+  value       = aws_s3_bucket.name.id
+}
